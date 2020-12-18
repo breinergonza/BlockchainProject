492cf3bead69b32f134cacaa4d14f89b7f283cb0
Breitner <breinergonza@hotmail.com>
2020-12-17 22:32:56 -0500

    Documentación del codigo

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -34,8 +34,8 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
       productCount = await marketplace.productCount()
     })
 
-    it('creates products', async () => {
-      // SUCCESS
+    it('crean los productos', async () => {
+      // Si es exitoso
       assert.equal(productCount, 1)
       const event = result.logs[0].args
       assert.equal(event.id.toNumber(), productCount.toNumber(), 'el nombre es correcto')
@@ -44,9 +44,9 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
       assert.equal(event.owner, seller, 'el propietario es correcto')
       assert.equal(event.purchased, false, 'comprado con exito')
 
-      // FAILURE: Product must have a name
+      // Error: El producto debe tener un nombre
       await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
-      // FAILURE: Product must have a price
+      // Error: El producto debe tener un precio
       await await marketplace.createProduct('Escritorio Virtual', 0, { from: seller }).should.be.rejected;
     })
 
@@ -60,23 +60,22 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
     })
 
     it('Vender productos', async () => {
-      // Track the seller balance before purchase
+      // Se le hace seguimiento al vendedor antes de la compra
       let oldSellerBalance
       oldSellerBalance = await web3.eth.getBalance(seller)
       oldSellerBalance = new web3.utils.BN(oldSellerBalance)
 
-      // SUCCESS: Buyer makes purchase
+      // Exitoso: El comprador realiza la compra del producto
       result = await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether')})
-
-      // Check logs
+     
       const event = result.logs[0].args
-      assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+      assert.equal(event.id.toNumber(), productCount.toNumber(), 'el id es correcto')
       assert.equal(event.name, 'Escritorio Virtual', 'name is correct')
       assert.equal(event.price, '1000000000000000000', 'el precio es correcto')
       assert.equal(event.owner, buyer, 'el propietario es correcto')
       assert.equal(event.purchased, true, 'comprado con exito')
 
-      // Check that seller received funds
+      // Se comprueba que el vendedor recibió los fondos
       let newSellerBalance
       newSellerBalance = await web3.eth.getBalance(seller)
       newSellerBalance = new web3.utils.BN(newSellerBalance)
@@ -89,13 +88,13 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
 
       assert.equal(newSellerBalance.toString(), exepectedBalance.toString())
 
-      // FAILURE: Tries to buy a product that does not exist, i.e., product must have valid id
-      await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;      // FAILURE: Buyer tries to buy without enough ether
-      // FAILURE: Buyer tries to buy without enough ether
+      // Error: Intenta comprar un producto que no existe, es decir, el producto debe tener una identificación válida
+      await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;      // Error: El comprador intenta comprar sin suficiente éter
+      // Error: El comprador intenta comprar sin suficiente éter
       await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether') }).should.be.rejected;
-      // FAILURE: Deployer tries to buy the product, i.e., product can't be purchased twice
+      // Error: El implementador intenta comprar el producto, es decir, el producto no se puede comprar dos veces
       await marketplace.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
-      // FAILURE: Buyer tries to buy again, i.e., buyer can't be the seller
+      // Error: El comprador intenta comprar de nuevo, es decir, el comprador no puede ser el vendedor.
       await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
     })
 
